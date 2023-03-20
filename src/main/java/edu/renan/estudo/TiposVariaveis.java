9ebd40b61fdf2a6d9b689fb9bc3076677e27b03b
Renan carneiro <carneiro.renan5@gmail.com>
2023-03-19 22:52:58 -0300

    feat: tests

--- src/main/java/edu/renan/estudo/TiposVariaveis.java
+++ src/main/java/edu/renan/estudo/TiposVariaveis.java
@@ -1,2 +1,11 @@
-package edu.renan.estudo;public class TiposVariaveis {
+package edu.renan.estudo;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        int a = 4,b = 10;
+        double media = (a + b) / 2;
+        String resultado = (media >= 7)? "aprovado" : "reprovado";
+
+        System.out.println(resultado);
+    }
 }
